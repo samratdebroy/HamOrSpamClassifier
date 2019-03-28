af1b630a54f0103bfc65d5dca2d73b2a83468d3f
samratdebroy <samratdebroy@users.noreply.github.com>
2019-03-28 16:34:09 -0400

    Finished Task 1 & 2 and Experiments 1,2,3
    Can run different experiments individually by calling different functions in experiments.py __main__

--- build_model.py
+++ build_model.py
@@ -1,61 +1,127 @@
 from collections import Counter
 import re
+import math
 
-# TASK 1: BUILD MODEL TEXT FILE
+def get_word_frequency():
+    # Prepare Counters to count the frequency of each word in each dataset
+    ham_counter = Counter()
+    spam_counter = Counter()
 
-# Prepare Counters to count the frequency of each word in each dataset
-ham_counter = Counter()
-spam_counter = Counter()
+    # Loop over all documents in the training set to obtain word frequency
+    num_ham_files = 1000
+    num_spam_files = 997
 
-# Prepare dict to track Conditional Probability for each word in each dataset
-spam_cond_prob = Counter()
-ham_cond_prob = Counter()
+    # Get probability that an email is either spam or ham
+    ham_prob = num_ham_files / (num_ham_files + num_spam_files)
+    spam_prob = num_spam_files / (num_ham_files + num_spam_files)
 
-# Loop over all documents in the training set to obtain word frequency
-num_ham_files = 1000
-num_spam_files = 997
+    # Count the frequency of words in the Ham database
+    for ham_file_id in range(1, num_ham_files + 1):
+        with open('Data/train/train-ham-{:05d}.txt'.format(ham_file_id),'r', encoding='latin-1') as file:
+            for line in file:
+                # Get the words in this line
+                words = re.split('[^a-zA-Z]', line.lower())
+                # Count the frequency of each non-empty word
+                non_empty_words = [word for word in words if word]
+                ham_counter.update(non_empty_words)
 
-# Count the frequency of words in the Ham database
-for ham_file_id in range(1, num_ham_files + 1):
-    with open('Data/train/train-ham-{:05d}.txt'.format(ham_file_id),'r') as file:
+    # Count the frequency of words in the Spam database
+    for spam_file_id in range(1, num_spam_files + 1):
+        with open('Data/train/train-spam-{:05d}.txt'.format(spam_file_id),'r', encoding='latin-1') as file:
+            for line in file:
+                # Get the words in this line
+                words = re.split('[^a-zA-Z]', line.lower())
+                # Count the frequency of each non-empty word
+                non_empty_words = [word for word in words if word]
+                spam_counter.update(non_empty_words)
+
+    return spam_counter, ham_counter, spam_prob, ham_prob
+
+def create_model(spam_counter, ham_counter, model_filename = 'model.txt'):
+
+    # Get the vocabulary of words present in the Training Set
+    vocab = list(set(spam_counter).union(set(ham_counter)))
+    vocab.sort()
+
+    # count number of words per data base
+    num_spam_words = sum(spam_counter.values())
+    num_ham_words = sum(ham_counter.values())
+
+    # Prepare Counters to track Conditional Probability for each word in each dataset
+    spam_cond_prob = Counter()
+    ham_cond_prob = Counter()
+
+    # Do smoothing and build conditional probabilities
+    delta = 0.5  # Smoothing amount per unique word
+    smoothed_spam_vocab_size = len(spam_counter)*delta
+    smoothed_ham_vocab_size = len(ham_counter)*delta
+    for word in vocab:
+        spam_cond_prob[word] = (spam_counter[word] + delta)/(num_spam_words + smoothed_spam_vocab_size)
+        ham_cond_prob[word] = (ham_counter[word] + delta)/(num_ham_words + smoothed_ham_vocab_size)
+
+    # Create and write the Model to file
+    with open(model_filename, 'w', encoding='latin-1') as model_file:
+        # Write each line of the model file
+        for line_num, word in enumerate(vocab, start=1):
+            line = '{num}  {word}  {ham_freq}  {ham_prob}  {spam_freq}  {spam_prob}\n'.format( num = line_num,
+                                            word = word, ham_freq = ham_counter[word], ham_prob = ham_cond_prob[word],
+                                            spam_freq = spam_counter[word], spam_prob = spam_cond_prob[word])
+            model_file.write(line)
+
+    return vocab, ham_cond_prob, spam_cond_prob
+
+def evaluate_model(ham_cond_prob, spam_cond_prob, spam_prob, ham_prob, results_filename = 'result.txt'):
+
+    num_ham_files = 400
+    num_spam_files = 400
+
+    # Create and write the Model to file
+    with open(results_filename, 'w', encoding='latin-1') as model_file:
+
+        # Loop through each file in the Test Ham database
+        for ham_file_id in range(1, num_ham_files + 1):
+            filename = 'test-ham-{:05d}.txt'.format(ham_file_id)
+            ham_score, spam_score, email_type = classify('Data/test/{}'.format(filename), ham_cond_prob,
+                                                         spam_cond_prob, spam_prob, ham_prob)
+
+            # Write each line of the model file
+            line = '{num}  {name}  {result}  {ham_score}  {spam_score}  {real_type}  {correct}\n'.format(
+                num=ham_file_id, name=filename, result=email_type, ham_score=ham_score, spam_score=spam_score,
+                real_type='ham',correct=(email_type == 'ham'))
+            model_file.write(line)
+
+        # Loop through each file in the Test Ham database
+        for spam_file_id in range(1, num_spam_files + 1):
+            filename = 'test-spam-{:05d}.txt'.format(spam_file_id)
+            ham_score, spam_score, email_type = classify('Data/test/{}'.format(filename), ham_cond_prob,
+                                                         spam_cond_prob, spam_prob, ham_prob)
+
+            # Write each line of the model file
+            line = '{num}  {name}  {result}  {ham_score}  {spam_score}  {real_type}  {correct}\n'.format(
+                num=spam_file_id + num_ham_files, name=filename, result=email_type, ham_score=ham_score,
+                spam_score=spam_score, real_type='spam',correct=(email_type == 'spam'))
+            model_file.write(line)
+
+
+def classify(email_filename, ham_cond_prob, spam_cond_prob, spam_prob, ham_prob):
+    ham_score = math.log10(ham_prob)
+    spam_score = math.log10(spam_prob)
+
+    with open(email_filename, 'r', encoding='latin-1') as file:
         for line in file:
             # Get the words in this line
             words = re.split('[^a-zA-Z]', line.lower())
-            # Count the frequency of each non-empty word
+
+            # Add the score from each word
             non_empty_words = [word for word in words if word]
-            ham_counter.update(non_empty_words)
+            for word in non_empty_words:
+                if ham_cond_prob[word]:
+                    ham_score += math.log10(ham_cond_prob[word])
+                if spam_cond_prob[word]:
+                    spam_score += math.log10(spam_cond_prob[word])
 
-# Count the frequency of words in the Spam database
-for spam_file_id in range(1, num_spam_files + 1):
-    with open('Data/train/train-spam-{:05d}.txt'.format(spam_file_id),'r') as file:
-        for line in file:
-            # Get the words in this line
-            words = re.split('[^a-zA-Z]', line.lower())
-            # Count the frequency of each non-empty word
-            non_empty_words = [word for word in words if word]
-            spam_counter.update(non_empty_words)
-
-# count number of words per data base
-num_spam_words = sum(spam_counter.values())
-num_ham_words = sum(ham_counter.values())
-
-# Get the vocabulary of words present in the Training Set
-vocab = list(set(spam_counter).union(set(ham_counter)))
-vocab.sort()
-
-# Do smoothing and build conditional probabilities
-delta = 0.5  # Smoothing amount per unique word
-smoothed_spam_vocab_size = len(spam_counter)*delta
-smoothed_ham_vocab_size = len(ham_counter)*delta
-for word in vocab:
-    spam_cond_prob[word] = (spam_counter[word] + delta)/(num_spam_words + smoothed_spam_vocab_size)
-    ham_cond_prob[word] = (ham_counter[word] + delta)/(num_ham_words + smoothed_ham_vocab_size)
-
-# Create and write the Model to file
-with open('model.txt', 'w') as model_file:
-    # Write each line of the model file
-    for line_num, word in enumerate(vocab, start=1):
-        line = '{num}  {word}  {ham_freq}  {ham_prob}  {spam_freq}  {spam_prob}\n'.format( num = line_num, word = word,
-                                                    ham_freq = ham_counter[word], ham_prob = ham_cond_prob[word],
-                                                    spam_freq = spam_counter[word], spam_prob = spam_cond_prob[word])
-        model_file.write(line)
+    if ham_score > spam_score:
+        email_type = 'ham'
+    else:
+        email_type = 'spam'
+    return ham_score, spam_score, email_type
---
+++ experiments.py
@@ -0,0 +1,74 @@
+import build_model
+
+def experiment1():
+    # Parse training data to build word frequencies
+    spam_counter, ham_counter, spam_prob, ham_prob = build_model.get_word_frequency()
+
+    # Create model of conditional probabilities
+    vocab, ham_cond_prob, spam_cond_prob = build_model.create_model(spam_counter, ham_counter,
+                                                                    model_filename="model.txt")
+
+    # Evaluate the effectiveness of the model over the test set
+    build_model.evaluate_model(ham_cond_prob, spam_cond_prob, spam_prob, ham_prob,
+                               results_filename='baseline-result.txt')
+
+
+def experiment2():
+    # Parse training data to build word frequencies
+    spam_counter, ham_counter, spam_prob, ham_prob = build_model.get_word_frequency()
+
+    # Get the stopwords
+    ham_words = sorted(ham_counter.keys())
+    spam_words = sorted(spam_counter.keys())
+    print('ham length:{} spam length:{}'.format(len(ham_counter), len(spam_counter)))
+    with open('Data/English-Stop-Words.txt', 'r', encoding='latin-1') as file:
+        for stopword in file:
+            # Remove stopwords from the counters
+            stopword = stopword[:-1]  # remove \n
+            if stopword in ham_words:
+                del ham_counter[stopword]
+            if stopword in spam_words:
+                del spam_counter[stopword]
+    print('ham length:{} spam length:{}'.format(len(ham_counter), len(spam_counter)))
+
+    # Create model of conditional probabilities
+    vocab, ham_cond_prob, spam_cond_prob = build_model.create_model(spam_counter, ham_counter,
+                                                                    model_filename="stopword-model.txt")
+
+    # Evaluate the effectiveness of the model over the test set
+    build_model.evaluate_model(ham_cond_prob, spam_cond_prob, spam_prob, ham_prob,
+                               results_filename='stopword-result.txt')
+
+def experiment3():
+    # Parse training data to build word frequencies
+    spam_counter, ham_counter, spam_prob, ham_prob = build_model.get_word_frequency()
+
+    # Get the vocabulary of words present in the Training Set
+    vocab = list(set(spam_counter).union(set(ham_counter)))
+    vocab.sort()
+
+    # Check if the word is the appropriate length
+    ham_words = sorted(ham_counter.keys())
+    spam_words = sorted(spam_counter.keys())
+    print('ham length:{} spam length:{}'.format(len(ham_counter), len(spam_counter)))
+    for word in vocab:
+        if len(word) <= 2 or  len(word) >= 9:
+            # Remove words of bad length from the counters
+            if word in ham_words:
+                del ham_counter[word]
+            if word in spam_words:
+                del spam_counter[word]
+    print('ham length:{} spam length:{}'.format(len(ham_counter), len(spam_counter)))
+
+
+    # Create model of conditional probabilities
+    vocab, ham_cond_prob, spam_cond_prob = build_model.create_model(spam_counter, ham_counter,
+                                                                    model_filename="wordlength-model.txt")
+
+    # Evaluate the effectiveness of the model over the test set
+    build_model.evaluate_model(ham_cond_prob, spam_cond_prob, spam_prob, ham_prob,
+                               results_filename='wordlength-result.txt')
+
+
+if __name__ == '__main__':
+    experiment3()
